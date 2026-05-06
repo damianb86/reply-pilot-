afaaf9e11d2e1b0a95dedf1bcd4eb371f56d70f9
Damian <damian@qdlagency.com>
2026-05-06 13:27:23 -0300

    robots

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,6 @@ export default function App() {
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <Meta />
         <Links />
       </head>
